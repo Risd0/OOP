13800d92f6fd43e8a7777aa6098e7f97dbdbb287
Nazar <55552780+Risd0@users.noreply.github.com>
2021-03-06 23:02:22 +0200

    Add files via upload

---
+++ OOP10/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace OOP10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rowsUpDown = new System.Windows.Forms.NumericUpDown();
+            this.colsUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.rowsUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colsUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Параметри матриці:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "К-сть рядків";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(13, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "К-сть стовпців";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(160, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Initialize matrix";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rowsUpDown
+            // 
+            this.rowsUpDown.Location = new System.Drawing.Point(118, 51);
+            this.rowsUpDown.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.rowsUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.rowsUpDown.Name = "rowsUpDown";
+            this.rowsUpDown.Size = new System.Drawing.Size(36, 20);
+            this.rowsUpDown.TabIndex = 6;
+            this.rowsUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.rowsUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.rowsUpDown.ValueChanged += new System.EventHandler(this.rowsUpDown_ValueChanged);
+            // 
+            // colsUpDown
+            // 
+            this.colsUpDown.Location = new System.Drawing.Point(118, 77);
+            this.colsUpDown.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.colsUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.colsUpDown.Name = "colsUpDown";
+            this.colsUpDown.Size = new System.Drawing.Size(36, 20);
+            this.colsUpDown.TabIndex = 6;
+            this.colsUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.colsUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.colsUpDown.ValueChanged += new System.EventHandler(this.rowsUpDown_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(300, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "The sum of non-negative columns:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(300, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(378, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Max із суми елементів діагоналей, паралельних головній:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 140);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(791, 314);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 466);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.colsUpDown);
+            this.Controls.Add(this.rowsUpDown);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rowsUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colsUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown rowsUpDown;
+        private System.Windows.Forms.NumericUpDown colsUpDown;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ OOP10/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP10
+{
+    public partial class Form1 : Form
+    {
+        int[,] arr;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Ok") { Application.Restart(); }
+
+            (int rows, int cols) = ((int)rowsUpDown.Value, (int)colsUpDown.Value);
+            arr = new int[rows, cols];
+
+            dataGridView1.RowCount = rows;
+            dataGridView1.ColumnCount = cols;
+
+            Randomize2DArr(-5, 11);
+
+            Print();
+
+            PositiveColsSum();
+
+            label5.Text += ParallelMainDiagMaxSum();
+
+            button1.Text = "Ok";
+        }
+
+        void Randomize2DArr(int min, int max)
+        {
+            var rand = new Random();
+            for (int i = 0; i < rowsUpDown.Value; i++)
+            {
+                for (int j = 0; j < colsUpDown.Value; j++)
+                {
+                    arr[i, j] = rand.Next(min, max);
+                }
+            }
+        }
+        void PositiveColsSum()
+        {
+            for (int j = 0; j < rowsUpDown.Value; j++)
+            {
+                int sum = 0;
+                bool isPositive = true;
+                for (int i = 0; i < colsUpDown.Value; i++)
+                {
+                    if (arr[i, j] >= 0) { sum += arr[i, j]; }
+                    else
+                    {
+                        isPositive = false;
+                        break;
+                    }
+
+                }
+                if (isPositive && sum != 0) { label4.Text += sum + " "; }
+            }
+        }
+        void Print()
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = arr[i, j];
+                }
+            }
+        }
+
+        int ParallelMainDiagMaxSum()
+        {
+            int size = (int)Math.Min(rowsUpDown.Value, colsUpDown.Value);
+            int max = arr[0, size - 1];
+            for (int k = -size + 1; k < size; ++k)
+            {
+                int sum = 0;
+                for (int i = Math.Max(0, k), j = Math.Max(-k, 0); j < Math.Min(size, size - k); i++, j++) { sum += arr[i, j]; }
+                if (max < sum) max = sum;
+            }
+            return max;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rowsUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            if (rowsUpDown.Value > 10) rowsUpDown.Value = 10;
+            else if (rowsUpDown.Value < 1) rowsUpDown.Value = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
